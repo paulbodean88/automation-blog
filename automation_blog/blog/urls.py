cb75f1e9d20ffbc790885e23c717104599428617
Eugen M <meltiseugen@gmail.com>
2017-11-02 23:10:34 +0200

    Description: added pagination and style from Paul

--- automation_blog/blog/urls.py
+++ automation_blog/blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^index', views.index, name='index'),
-    url(r'^home', views.home, name='home'),
+    # url(r'^home', views.home, name='index1'),
+    url(r'^home(/[0-9]*)+/', views.home, name='home'),
 ]
 
--- automation_blog/blog/views.py
+++ automation_blog/blog/views.py
@@ -1,6 +1,10 @@
+from typing import List
+
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
+
+from .models import Post
 
 
 def index(request):
@@ -11,7 +15,27 @@ def index(request):
     return HttpResponse(template.render(data, request))
 
 
-def home(request):
-    # template = loader.get_template('articles/home.html')
+def home(request, page=None):
+    print(type(page))
+    page = 0 if page is None else int(page[1:])
+    posts: List[Post] = Post.objects.all().order_by("-created_date")
+
+    render_posts = []
+    for post in posts[page * 3: page * 3 + 3]:
+        render_posts.append({
+                'tile': post.title,
+                'body': post.body
+            })
+
+    next_page_index = page + 1
+    if page * 3 + 3 >= len(posts):
+        next_page_index = -1
+
+    response = {
+        'posts': render_posts,
+        'next_page': next_page_index,
+        'prev_page': page - 1
+    }
+
     template = loader.get_template('index.html')
-    return HttpResponse(template.render())
+    return HttpResponse(template.render(response))
